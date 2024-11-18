abf45a32f374bdb01bf2ecc7a6ee0065978d498a
DanhUET <22022201@vnu.edu.vn>
2024-11-18 13:31:10 +0700

    db

--- backend/controllers/pens/deletedPen.js
+++ backend/controllers/pens/deletedPen.js
@@ -1,28 +1,26 @@
-const db = require("../../config/db")
-
+const db = require('../../config/db');
+//Delete pen
 async function deletedPen(req, res) {
     try {
-        db.query('DELETE FROM pens WHERE id = ?', [req.params.id],
-            function(err, result) {
-                if (err) {
-                    return res.json({
-                        success: false,
-                        message: err.message
-                    })
-                }
-
-                res.json({
-                    success: true,
-                    message: 'Deleted successfully'
-                })
+        db.query('DELETE FROM pens WHERE id = ?', [req.params.id], function (err, result) {
+            if (err) {
+                return res.json({
+                    success: false,
+                    message: err.message,
+                });
             }
-        )
+
+            res.json({
+                success: true,
+                message: 'Deleted successfully',
+            });
+        });
     } catch (err) {
         return res.json({
             message: err.message,
-            success: false
-        })
+            success: false,
+        });
     }
-} 
+}
 
-module.exports = deletedPen
+module.exports = deletedPen;
--- backend/controllers/pens/updatePen.js
+++ backend/controllers/pens/updatePen.js
@@ -1,28 +1,32 @@
-const db = require("../../config/db")
+const db = require('../../config/db');
+//undatePen
 
 async function updatePen(req, res) {
-    const { html, css, js, output, title } =  req.body;
+    const { html, css, js, output, title } = req.body;
     try {
-        db.query('UPDATE pens SET html = ?, css =?, js =?, output= ?, title =? WHERE id = ?', [html, css, js, output, title, req.params.id],
-            function(err, result) {
+        db.query(
+            'UPDATE pens SET html = ?, css =?, js =?, output= ?, title =? WHERE id = ?',
+            [html, css, js, output, title, req.params.id],
+            function (err, result) {
                 if (err) {
                     return res.status(500).json({
                         message: err.message,
-                        success: false
-                    })
+                        success: false,
+                    });
                 }
                 res.status(200).json({
                     success: true,
-                    message: "Pens fetched successfully",
-                    data: result
-                })
-            }
-        )
+                    message: 'Pens fetched successfully',
+                    data: result,
+                });
+            },
+        );
+        //
     } catch (err) {
         res.json({
             message: err.message,
-            success: false
-        })
+            success: false,
+        });
     }
 }
 
