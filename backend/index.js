713f296231607b201c36879e8d320b54dd4f7c94
TKhawns <trinhkhanhctdt@gmail.com>
2024-12-08 23:20:02 +0700

    test deploy

--- backend/index.js
+++ backend/index.js
@@ -3,20 +3,25 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const router = require("./routes");
+const path = require('path');
 
 const app = express();
 
 app.use(express.json());
-app.use(
-	cors({
-		origin: process.env.FRONTEND_URL,
-		credentials: true,
-	})
-);
+app.use(cors());
 
 app.use("/api", router);
 
-const PORT = 8080;
+// Serve static files from the React app's build folder
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Catch-all handler to serve React's index.html for any unknown route
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
