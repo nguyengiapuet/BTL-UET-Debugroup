f7ddf8ed7692c1e715cf5842f6b730d87dd47c69
DanhUET <22022201@vnu.edu.vn>
2024-12-11 12:05:53 +0700

    .

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -130,5 +130,6 @@ router.post("/mark-as-read-notification", verifyToken, markAsReadNotification);
 //question
 router.get("/learn/question1", verifyToken, getQuestion1);
 router.get("/learn/question2", verifyToken, getQuestion2);
-
+// router.get("/learn/question1", verifyToken, getQuestion1);
+// router.get("/learn/question2", verifyToken, getQuestion2);
 module.exports = router;
